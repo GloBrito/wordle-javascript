9873a5cf106b230759a6b28355960dda5eeb085f
Glória Brito <gloriafbrito@gmail.com>
2022-08-19 11:50:16 -0300

    add squares

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => { //registra uma única espera de evento em um único alvo (ouvinte de eventos).
-    createSquares();
+ createSquares();
 
+//Adiciona os quadrados (square)
     function createSquares(){
         const gameBoard = document.getElementById("board") //pega o elemento pelo id que será o tabuleiro
 
         for (let index = 0; index < 30; index++){ //30 quadrados do tabuleiro
-            let square = document.createElement("div") //cria o elemento HTML especificado
-            square.setAttribute("id", index + 1)  //Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico. Id do número do quadrado.
-            gameBoard.appendChild(square)
-
+            let square = document.createElement("div"); //cria o elemento HTML especificado
+            square.classList.add("square");
+            square.setAttribute("id", index +1); //Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico. Id do número do quadrado.
+            gameBoard.appendChild(square);
         }
     }
+    });
