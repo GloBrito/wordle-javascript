6cd6c139f4cb0ea66a30020f65b25c99c2ff6709
Glória Brito <gloriafbrito@gmail.com>
2022-08-19 15:47:50 -0300

    acrescenta lettras

--- app.js
+++ app.js
@@ -1,22 +1,162 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //registra uma única espera de evento em um único alvo (ouvinte de eventos).
-  createSquares();
-
-  //Adiciona os quadrados (square)
-  function createSquares() {
-    const gameBoard = document.getElementById("board"); //pega o elemento pelo id que será o tabuleiro
-
-    for (let index = 0; index < 30; index++) {
-      //30 quadrados do tabuleiro
-
-      let square = document.createElement("div"); //cria o elemento HTML especificado
-      square.classList.add("square");
-      square.setAttribute("id", index + 1); //Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico. Id do número do quadrado.
-      gameBoard.appendChild(square);
-      // if ((index + 1) % 5 == 0){
-      //     gameBoard.innerHTML += '<div>g</div>';
-      // }else{
-      // }
+    createSquares();
+    getNewWord();
+  
+    let guessedWords = [[]];
+    let availableSpace = 1;
+  
+    let word;
+    let guessedWordCount = 0;
+  
+    const keys = document.querySelectorAll(".keyboard-row button");
+  
+    function getNewWord() {
+      fetch(
+        `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          word = res.word;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     }
-  }
-});
+  
+    function getCurrentWordArr() {
+      const numberOfGuessedWords = guessedWords.length;
+      return guessedWords[numberOfGuessedWords - 1];
+    }
+  
+    function updateGuessedWords(letter) {
+      const currentWordArr = getCurrentWordArr();
+  
+      if (currentWordArr && currentWordArr.length < 5) {
+        currentWordArr.push(letter);
+  
+        const availableSpaceEl = document.getElementById(String(availableSpace));
+  
+        availableSpace = availableSpace + 1;
+        availableSpaceEl.textContent = letter;
+      }
+    }
+  
+    function getTileColor(letter, index) {
+      const isCorrectLetter = word.includes(letter);
+  
+      if (!isCorrectLetter) {
+        return "rgb(58, 58, 60)";
+      }
+  
+      const letterInThatPosition = word.charAt(index);
+      const isCorrectPosition = letter === letterInThatPosition;
+  
+      if (isCorrectPosition) {
+        return "rgb(83, 141, 78)";
+      }
+  
+      return "rgb(181, 159, 59)";
+    }
+  
+    function handleSubmitWord() {
+      const currentWordArr = getCurrentWordArr();
+      if (currentWordArr.length !== 5) {
+        window.alert("A palavra deve ter 5 letras");
+      }
+  
+      const currentWord = currentWordArr.join("");
+  
+      fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+          "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
+        },
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error();
+          }
+  
+          const firstLetterId = guessedWordCount * 5 + 1;
+          const interval = 200;
+          currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+              const tileColor = getTileColor(letter, index);
+  
+              const letterId = firstLetterId + index;
+              const letterEl = document.getElementById(letterId);
+              letterEl.classList.add("animate__flipInX");
+              letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index);
+          });
+  
+          guessedWordCount += 1;
+  
+          if (currentWord === word) {
+            window.alert("Parabéns!");
+          }
+  
+          if (guessedWords.length === 6) {
+            window.alert(`Desculpe, você não tem mais palpites! A palavra é ${word}.`);
+          }
+  
+          guessedWords.push([]);
+        })
+        .catch(() => {
+          window.alert("A palavra não é reconhecida!");
+        });
+    }
+  
+    function createSquares() {
+      const gameBoard = document.getElementById("board");
+  
+      for (let index = 0; index < 30; index++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("animate__animated");
+        square.setAttribute("id", index + 1);
+        gameBoard.appendChild(square);
+      }
+    }
+  
+    function handleDeleteLetter() {
+      const currentWordArr = getCurrentWordArr();
+      const removedLetter = currentWordArr.pop();
+  
+      guessedWords[guessedWords.length - 1] = currentWordArr;
+  
+      const lastLetterEl = document.getElementById(String(availableSpace - 1));
+  
+      lastLetterEl.textContent = "";
+      availableSpace = availableSpace - 1;
+    }
+  
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].onclick = ({ target }) => {
+        const letter = target.getAttribute("data-key");
+  
+        if (letter === "enter") {
+          handleSubmitWord();
+          return;
+        }
+  
+        if (letter === "del") {
+          handleDeleteLetter();
+          return;
+        }
+  
+        updateGuessedWords(letter);
+      };
+    }
+  });
+  
